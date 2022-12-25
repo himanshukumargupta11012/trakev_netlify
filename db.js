78dff3a8ce84b09f8dee2246cdf77ed67a2d0c46
Himanshu Kumar Gupta <ai21btech11012@iith.ac.in>
2022-12-25 11:42:34 +0530

    conneted with atlas

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb://localhost:27017/EV_login_info",{
-}).then(()=>
+mongoose.connect("mongodb+srv://ev_administrator:jRPXuMmvYSVkc3De@ev-info.0un5qou.mongodb.net/EV-info?retryWrites=true&w=majority").then(()=>
     console.log("connection successful")
 ).catch((err)=>console.log("404 page not found")
 
